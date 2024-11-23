ab4590fe4f63784b870fb8f4cbfbd77a04fcfc42
AimenAlamin <alaminaimen@gmail.com>
2024-11-23 21:02:25 +0200

    added apiSerivce Class

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,8 +1,15 @@
 class BookEntity {
-  final String image;
+  final String bookId;
+  final String? image;
   final String title;
-  final String authorName;
-  final num price;
-  final num rating;
-  BookEntity(this.image, this.title, this.authorName, this.price, this.rating);
+  final String? authorName;
+  final num? price;
+  final num? rating;
+  BookEntity(
+      {required this.image,
+      required this.bookId,
+      required this.title,
+      required this.authorName,
+      required this.price,
+      required this.rating});
 }
