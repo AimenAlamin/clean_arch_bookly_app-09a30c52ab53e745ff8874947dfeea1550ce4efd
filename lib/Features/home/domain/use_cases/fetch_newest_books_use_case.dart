109adec0e4341ab1a1452ae0c8c4bc699ac84c7a
AimenAlamin <alaminaimen@gmail.com>
2024-11-23 05:45:38 +0200

    finished domain layer

---
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,12 @@
+import '../../../../core/use_cases/use_case.dart';
+import '../../entities/book_entity.dart';
+import '../../repos/home_repo.dart';
+
+class FetchNewestdBooksUseCase extends UseCase<List<BookEntity>, void> {
+  final HomeRepo homeRepo;
+  FetchNewestdBooksUseCase(this.homeRepo);
+  @override
+  Future<List<BookEntity>> call([void param]) async {
+    return await homeRepo.fetchNewestBooks();
+  }
+}
