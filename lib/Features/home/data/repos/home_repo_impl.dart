67322337ee2ec8483bbf8fa05738a941e17b1cfa
AimenAlamin <alaminaimen@gmail.com>
2024-11-24 05:46:56 +0200

    home repo Implementation

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,55 @@
+import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly/core/errors/faliure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource remoteDataSource;
+  final HomeLocalDataSource localDataSource;
+  HomeRepoImpl({required this.remoteDataSource, required this.localDataSource});
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = localDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await remoteDataSource.fetchFeaturedBooks();
+      return right(books);
+      // var localBooks = localDataSource.fetchFeaturedBooks();
+      // if(localBooks.isNotEmpty){
+      //   return Right(localBooks);
+      // }
+      // var remoteBooks = await remoteDataSource.fetchFeaturedBooks();
+      // return Right(remoteBooks);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      var localBooks = localDataSource.fetchNewestBooks();
+      if (localBooks.isNotEmpty) {
+        return right(localBooks);
+      }
+      var remoteBooks = await remoteDataSource.fetchNewestBooks();
+      return Right(remoteBooks);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
--- lib/core/errors/faliure.dart
+++ lib/core/errors/faliure.dart
@@ -1,1 +1,46 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+  Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(e.response!);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request Cancelled');
+      case DioExceptionType.unknown:
+        return ServerFailure('Something went wrong');
+      default:
+        return ServerFailure('Something went wrong');
+    }
+  }
+  factory ServerFailure.fromResponse(Response response) {
+    switch (response.statusCode) {
+      case 400:
+        return ServerFailure('Bad Request');
+      case 401:
+        return ServerFailure('Unauthorized');
+      case 403:
+        return ServerFailure('Forbidden');
+      case 404:
+        return ServerFailure('Not Found');
+      case 500:
+        return ServerFailure('Internal Server Error');
+      default:
+        return ServerFailure('Something went wrong');
+    }
+  }
+}
